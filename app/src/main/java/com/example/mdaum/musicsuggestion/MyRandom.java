2c4ccd72296a80325ecd8477f69fe0cf27b59b12
unknown <daumisgod@gmail.com>
2017-04-25 16:36:55 -0400

    better naming of vars and spacing...commented etc

--- app/src/main/java/com/example/mdaum/musicsuggestion/MyRandom.java
+++ app/src/main/java/com/example/mdaum/musicsuggestion/MyRandom.java
@@ -2,16 +2,16 @@ package com.example.mdaum.musicsuggestion;
 
 import java.util.*;
 
-public class MyRandom {
+public class MyRandom { //just easier to use than Rand...
 
   private Random rn;
 
-  public MyRandom(boolean Seeded){
+  public MyRandom(boolean Seeded){ //seeded is used for research purposes...do I want to generate same psuedo-random sequence or not?
 	  rn=new Random();
 	  if(Seeded)rn.setSeed(100);
   }
 
-  public  int rand(int lo, int hi) {
+  public  int rand(int lo, int hi) { //lo is inclusive, hi is exclusive
      int n = hi - lo + 1;
      int i = rn.nextInt() % n;
      if (i < 0) i = -i;
@@ -19,7 +19,7 @@ public class MyRandom {
   }
 
   @SuppressWarnings("deprecation")
-public  String nextString(int lo, int hi) {
+public  String nextString(int lo, int hi) { //random a-z string of random size b/w lo (inclusive) and hi (exclusive)
      int n = rand(lo, hi);
      byte b[] = new byte[n];
      for (int i = 0; i < n; i++)
